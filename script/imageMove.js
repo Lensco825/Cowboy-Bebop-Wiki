62cb643914d31d1cd09926431f4dcc03c177af9c
Lens D. <lensdorsainvil1@gmail.com>
2024-12-11 14:16:43 -0500

    Added mouse move image effect to cinematography page

---
+++ script/imageMove.js
@@ -0,0 +1,21 @@
+const exampleImages = document.querySelectorAll('#exampleImage');
+const heroSection = document.querySelector('.hero-section');
+
+
+
+exampleImages.forEach(example => {
+    const elementStyle = window.getComputedStyle(example);
+    const xPosition = elementStyle.getPropertyValue("left");
+    const yPosition = elementStyle.getPropertyValue("bottom");     
+    heroSection.addEventListener('mousemove', (e) => {
+       let mouseX = e.pageX;
+        let mouseY = e.pageY;
+        let offSetX = example.getBoundingClientRect().width / 2;
+        let offSetY = example.getBoundingClientRect().height / 2;
+        example.style.left = `calc(${xPosition} - ${mouseX - offSetX}px * 0.1)`;
+        example.style.top = `calc(${yPosition} - ${mouseY - offSetY}px * 0.1)`;
+        console.log(String(xPosition));
+    });
+    
+})
+
