20fb7064d5a143f6fdcfd3d44c60c1a6c9f32b44
Lens D. <lensdorsainvil1@gmail.com>
2025-01-13 13:37:56 -0500

    Slowed down image movement in characters page

--- script/imageMove.js
+++ script/imageMove.js
@@ -12,8 +12,8 @@ exampleImages.forEach(example => {
         let mouseY = e.pageY;
         let offSetX = example.getBoundingClientRect().width / 2;
         let offSetY = example.getBoundingClientRect().height / 2;
-        example.style.left = `calc(${xPosition} - ${mouseX - offSetX}px * 0.1)`;
-        example.style.top = `calc(${yPosition} - ${mouseY - offSetY}px * 0.1)`;
+        example.style.left = `calc(${xPosition} - ${mouseX - offSetX}px * 0.03)`;
+        example.style.top = `calc(${yPosition} - ${mouseY - offSetY}px * 0.03)`;
         console.log(String(xPosition));
     });
     
